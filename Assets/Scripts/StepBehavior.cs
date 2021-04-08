3aaad37fb2b86e54c1130da7b064d18ac2893b02
Anton Larkin <anton11061998@gmail.com>
2021-04-08 03:26:05 +0300

    init task 1-4

---
+++ Assets/Scripts/StepBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StepBehavior : MonoBehaviour
+{
+    #region Variables
+    [TextArea(15,50)]
+    public string contentText;
+    public string locationName ;
+
+    public StepBehavior[] nextSteps;
+
+    public Text currentLocation;
+
+    #endregion
+
+    #region Unity lifecycle
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    #region Variables
+    public Text contentLabel;
+    public Text locationLabel;
+    public StepBehavior startStep;
+
+    private StepBehavior currentStep;
+
+    #endregion
+
+    #region Unity lifecycle
+    void Start()
+    {
+        currentStep = startStep;
+        UpdateCurrentLabel();
+        UpdateCurrentLocation(startStep.locationName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            MoveToNextStep(0);
+            UpdateCurrentLocation(currentStep.locationName);
+            //currentStep.currentLocation.text
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MoveToNextStep(1);
+            UpdateCurrentLocation(currentStep.locationName);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            MoveToNextStep(2);
+            UpdateCurrentLocation(currentStep.locationName);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            MoveToNextStep(3);
+            UpdateCurrentLocation(currentStep.locationName);
+        }
+
+    }
+
+    #endregion
+
+    #region Private methods
+    private void UpdateCurrentLabel()
+    {
+        contentLabel.text = currentStep.contentText;
+    }
+    private void UpdateCurrentLocation(string currentLocation)
+    {
+        locationLabel.text = currentLocation;
+    }
+    private void MoveToNextStep(int stepIndex)
+    {
+        if (currentStep.nextSteps.Length > stepIndex)
+        {
+            currentStep = currentStep.nextSteps[stepIndex];
+            UpdateCurrentLabel();
+        }
+    }
+    #endregion
+}
